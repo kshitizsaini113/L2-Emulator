0f83ae06e8561b19f89f9c7132a7cb9d4b30658e
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2020-10-01 08:29:41 +0530

    Updating network properties

--- Project_Implementation/main.c
+++ Project_Implementation/main.c
@@ -5,6 +5,6 @@ extern network_graph_t *build_first_network_topology();
 int main(int argc, char **argv)
 {
     network_graph_t *network_topology = build_first_network_topology();
-    dump_network_graph(network_topology);
+    dump_network_graph_net(network_topology);
     return 0;
 }
--- Project_Implementation/network_topology.c
+++ Project_Implementation/network_topology.c
@@ -12,6 +12,22 @@ network_graph_t * build_first_network_topology()
     insert_link_between_two_network_nodes(router_1, router_2, "eth0/2", "eth0/3", 1);
     insert_link_between_two_network_nodes(router_0, router_2, "eth0/4", "eth0/5", 1);
 
+    network_node_device_type(router_0, L3_ROUTER);
+    network_node_set_loopback_address(router_0, "122.1.1.0");
+    network_node_set_interface_ip_address(router_0, "eth0/4", "40.1.1.1", 24);
+    network_node_set_interface_ip_address(router_0, "eth0/0", "20.1.1.1", 24);
+
+    network_node_device_type(router_1, L3_ROUTER);
+    network_node_set_loopback_address(router_1, "122.1.1.1");
+    network_node_set_interface_ip_address(router_1, "eth0/1", "20.1.1.1", 24);
+    network_node_set_interface_ip_address(router_1, "eth0/2", "30.1.1.1", 24);
+
+    network_node_device_type(router_2, L3_ROUTER);
+    network_node_set_loopback_address(router_2, "122.1.1.2");
+    network_node_set_interface_ip_address(router_2, "eth0/3", "30.1.1.1", 24);
+    network_node_set_interface_ip_address(router_2, "eth0/5", "40.1.1.1", 24);
+
+
     return network_topology_1;
 }
 
