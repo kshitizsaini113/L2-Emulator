da0147a4a9508e30488333034eb779e694c09d7a
Pratyusha Agarwal <63654022+pratyusha1098-pa@users.noreply.github.com>
2020-10-04 22:07:54 +0530

    Added command line interface via LibCli

--- Project_Implementation/main.c
+++ Project_Implementation/main.c
@@ -1,10 +1,14 @@
 #include "networkgraph.h"
+#include "LibCli/libcli.h"
 
 extern network_graph_t *build_first_network_topology();
+extern void initialize_network_cli();
+network_graph_t *network_topology = NULL;
 
 int main(int argc, char **argv)
 {
-    network_graph_t *network_topology = build_first_network_topology();
-    dump_network_graph_net(network_topology);
+    initialize_network_cli();
+    network_topology = build_first_network_topology();
+    start_shell();
     return 0;
 }
